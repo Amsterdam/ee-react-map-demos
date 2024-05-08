0fe04a10e40d20f25e522ae684d47527f350f565
Thomas Mills <mills.thoma@gmail.com>
2024-05-08 16:47:12 +0200

    BaseLayer example setup for Leaflet

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,12 @@
 import type { RouteObject } from 'react-router-dom';
 import App from '@/containers/App';
-import Home from '@/pages/Home';
+import BaseLayer from '@/pages/BaseLayer';
 import Contact from '@/pages/Contact';
 
 const children: RouteObject[] = [
   {
     path: '/',
-    element: <Home />,
+    element: <BaseLayer />,
   },
   {
     path: 'contact',
