801937749d43e60ca66d7f68c9bc557756ad725f
Niels Roozemond <n.roozemond@amsterdam.nl>
2024-05-21 13:51:09 +0200

    Some design system storybook config

---
+++ .storybook/preview.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '@amsterdam/design-system-tokens/dist/index.css';
+import '@amsterdam/design-system-tokens/dist/compact.theme.css';
+import '@amsterdam/design-system-assets/font/index.css';
+import '@amsterdam/design-system-css/dist/index.css';
+import { withThemeByClassName } from '@storybook/addon-themes';
+import { viewports } from './viewports';
+
+// Set language to Dutch for Canvas and Stories
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      Spacious: '',
+      Compact: 'ams-theme--compact',
+    },
+    defaultTheme: 'Spacious',
+  }),
+];
+
+export const parameters = {
+  backgrounds: {
+    disable: true,
+  },
+  viewport: {
+    viewports,
+  },
+};
