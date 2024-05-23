6d90af88ff05bc3142996d98c93637c38b11a0a4
Thomas Mills <mills.thoma@gmail.com>
2024-05-23 10:51:54 +0200

    BaseLayer index files renamed (#13)
    * BaseLayer index files renamed

--- src/stories/pages/BaseLayer/index.stories.ts
+++ src/stories/pages/BaseLayer/index.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import BaseLayer from '@/pages/BaseLayer';
+import BaseLayer from '@/pages/BaseLayer/BaseLayer';
 
 const meta = {
   title: 'React/BaseLayer',
