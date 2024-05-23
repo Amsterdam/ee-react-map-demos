1d4da4f69b74b7a303019a17f48ea88cead95a05
Thomas Mills <mills.thoma@gmail.com>
2024-05-23 15:59:44 +0200

    Feature/scts 2 pointer marker (#11) (#20)
    * Leaflet marker example setup
    
    * Marker docs and docs for Leaflet icons added

---
+++ src/pages/Marker/Marker.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useRef, useState } from 'react';
+import type { FunctionComponent } from 'react';
+import L from 'leaflet';
+import getCrsRd from '@/utils/getCrsRd';
+import styles from './styles.module.css';
+import customMarker from './icons/customMarker';
+
+const Marker: FunctionComponent = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [mapInstance, setMapInstance] = useState<L.Map | null>(null);
+  const [, setMarkerInstance] = useState<L.Marker | null>(null);
+  const createdMapInstance = useRef(false);
+
+  // Set the Leaflet map and Amsterdam base layer
+  useEffect(() => {
+    if (containerRef.current === null || createdMapInstance.current !== false) {
+      return;
+    }
+
+    const map = new L.Map(containerRef.current, {
+      center: L.latLng([52.370216, 4.895168]),
+      zoom: 12,
+      layers: [
+        L.tileLayer('https://{s}.data.amsterdam.nl/topo_rd/{z}/{x}/{y}.png', {
+          attribution: '',
+          subdomains: ['t1', 't2', 't3', 't4'],
+          tms: true,
+        }),
+      ],
+      zoomControl: false,
+      maxZoom: 16,
+      minZoom: 3,
+      crs: getCrsRd(),
+      maxBounds: [
+        [52.25168, 4.64034],
+        [52.50536, 5.10737],
+      ],
+    });
+
+    map.attributionControl.setPrefix(false);
+
+    createdMapInstance.current = true;
+    setMapInstance(map);
+
+    return () => {
+      if (mapInstance) mapInstance.remove();
+    };
+  }, []);
+
+  // Create the marker and add it to the map
+  useEffect(() => {
+    if (mapInstance) {
+      const marker = L.marker([52.370216, 4.895168], {
+        // There are many more options to choose from @see https://leafletjs.com/reference.html#marker
+        icon: customMarker,
+      })
+        .addTo(mapInstance)
+        // Marker click event listener example
+        .on('click', () => alert('Marker click!'));
+      setMarkerInstance(marker);
+    }
+  }, [mapInstance]);
+
+  return <div className={styles.container} ref={containerRef} />;
+};
+
+export default Marker;
