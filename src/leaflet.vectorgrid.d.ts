c7217561e81d64f0f43541dee10ca9bba64a42ed
Thomas Mills <mills.thoma@gmail.com>
2024-05-27 17:01:55 +0200

    WIP MVT layer

---
+++ src/leaflet.vectorgrid.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// in the global namesapce "L"
+declare namespace L {
+  // there is a child namespace "vectorGrid"
+  namespace vectorGrid {
+    // which has a function call "slicer" that takes data and optional
+    // configurations. To make it simple, we don't specify the input
+    // and output types.
+    export function slicer(data: any, options?: any): any;
+    export function protobuf(data: any, options?: any): any;
+  }
+}
---
+++ src/pages/MVTLayer/MVTLayer.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useRef, useState } from 'react';
+import type { FunctionComponent } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import getCrsRd from '@/utils/getCrsRd';
+import styles from './styles.module.css';
+
+const WMSLayer: FunctionComponent = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [mapInstance, setMapInstance] = useState<L.Map | null>(null);
+  const createdMapInstance = useRef(false);
+
+  // Set the Leaflet map and Amsterdam base layer
+  useEffect(() => {
+    if (containerRef.current === null || createdMapInstance.current !== false) {
+      return;
+    }
+
+    const map = new L.Map(containerRef.current, {
+      center: L.latLng([52.370216, 4.895168]),
+      zoom: 12,
+      layers: [
+        L.tileLayer('https://{s}.data.amsterdam.nl/topo_rd/{z}/{x}/{y}.png', {
+          attribution: '',
+          subdomains: ['t1', 't2', 't3', 't4'],
+          tms: true,
+        }),
+      ],
+      zoomControl: false,
+      maxZoom: 16,
+      minZoom: 3,
+      crs: getCrsRd(),
+      maxBounds: [
+        [52.25168, 4.64034],
+        [52.50536, 5.10737],
+      ],
+    });
+
+    map.attributionControl.setPrefix(false);
+
+    createdMapInstance.current = true;
+    setMapInstance(map);
+
+    return () => {
+      if (mapInstance) mapInstance.remove();
+    };
+  }, []);
+
+  // Create the MVT layer and add it to the map
+  useEffect(() => {
+    if (mapInstance === null) {
+      return;
+    }
+
+    L.vectorGrid
+      .protobuf(
+        'https://api.data.amsterdam.nl/v1/mvt/afvalwijzer/afvalwijzer/{z}/{x}/{y}.pbf'
+      )
+      .addTo(mapInstance);
+  }, [mapInstance]);
+
+  return <div className={styles.container} ref={containerRef} />;
+};
+
+export default WMSLayer;
