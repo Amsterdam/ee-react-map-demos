077a86c4f675f54ff8c0ef7a41a48d51f7b46511
Thomas Mills <mills.thoma@gmail.com>
2024-05-29 11:20:43 +0200

    WMSLayer example, test and docs (#23)

---
+++ src/stories/pages/WMSLayer/index.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import BaseLayer from '@/pages/WMSLayer/WMSLayer';
+
+const meta = {
+  title: 'React/WMSLayer',
+  component: BaseLayer,
+  parameters: {
+    layout: 'fullscreen',
+    options: {
+      panelPosition: 'bottom',
+      bottomPanelHeight: 0,
+    },
+  },
+} satisfies Meta<typeof BaseLayer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
