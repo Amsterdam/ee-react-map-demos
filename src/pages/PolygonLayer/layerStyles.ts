056bd68e130c6589248009bd12735de1bf611620
Thomas Mills <mills.thoma@gmail.com>
2024-06-05 13:17:07 +0200

    [3] Polygon marker (#21)
    * Working polygonlayer and test
    
    * Polygon layer docs and styles separated

---
+++ src/pages/PolygonLayer/layerStyles.ts
@@ -0,0 +1,10 @@
+export const polygonStyles = {
+  fillOpacity: 0.2,
+  color: '#0000ff',
+};
+
+export const polygonHoverStyles = {
+  ...polygonStyles,
+  fillOpacity: 0.5,
+  color: '#ff0000',
+};
