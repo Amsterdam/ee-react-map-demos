73491f84c32298a77d76c4c70faf5c33d47b77ed
Thomas Mills <mills.thoma@gmail.com>
2024-07-15 14:54:50 +0200

    Remove unnused footer component file and file reference in docs

--- src/pages/ContextExamples/ZoomControlsFullScreen/Footer.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import Footer from './Footer';
-
-describe('ZoomControlsFullScreen Footer', () => {
-  it('renders the component', () => {
-    const { container } = render(<Footer />);
-    expect(container.firstChild).toBeDefined();
-  });
-});
