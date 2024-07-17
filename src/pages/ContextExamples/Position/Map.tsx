2bc5e51d654217dcf4331ab60e256b3326ae688d
Thomas Mills <mills.thoma@gmail.com>
2024-07-17 11:48:14 +0200

    fix min and max zoom levels

--- src/pages/ContextExamples/Position/Map.tsx
+++ src/pages/ContextExamples/Position/Map.tsx
@@ -30,7 +30,7 @@ const Map: FunctionComponent = () => {
       ],
       zoomControl: false,
       maxZoom: 16,
-      minZoom: 3,
+      minZoom: 6,
       crs: getCrsRd(),
       maxBounds: [
         [52.25168, 4.64034],
