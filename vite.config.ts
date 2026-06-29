4099332b4414c5a57ce9d655ccc3355702805326
Thomas Mills <mills.thoma@gmail.com>
2026-06-29 15:49:10 +0200

    typescript bumped to 6.0.3

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-/// <reference types="vitest" />
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react(), svgr()],
   test: {
     globals: true,
